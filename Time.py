6a9dd27d87693d8201351e66d98c26912eb7c65c
hmcgough <hmcgough@eecs.wsu.edu>
2019-11-03 06:50:18 -0800

    change refresh rate

--- Time.py
+++ Time.py
@@ -27,12 +27,14 @@ class Time:
     def monitor_time(self):
         while True:
             new_time = datetime.datetime.now()
-            if self.cur_time.time().second != new_time.time().second:
+            if self.cur_time.time().minute != new_time.time().minute:
                 self.cur_time = new_time
                 for item in self.toNotify:
                     item.notify(self)
 
             self.cur_time = new_time
+
+            Thread.sleep(.5)
 
 
 
