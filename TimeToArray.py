30978294a7f6c8bdbad56a4b5ab0d3003ee738aa
hmcgough <hmcgough@eecs.wsu.edu>
2019-11-03 05:50:11 -0800

    update every second

--- TimeToArray.py
+++ TimeToArray.py
@@ -2,9 +2,9 @@
 def time_to_array(time):
     time_array = [None] * 4
 
-    time_array[1] = (time.minute() / 10) % 10
-    time_array[2] = time.minute() % 10
-    time_array[3] = (time.second() / 10) % 10
-    time_array[4] = time.second() % 10
+    time_array[1] = (time.minute / 10) % 10
+    time_array[2] = time.minute % 10
+    time_array[3] = (time.second / 10) % 10
+    time_array[4] = time.second % 10
 
     return time_array
