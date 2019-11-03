08c6501ea1ef4c82a102a83729802b8cddf0bad4
hmcgough <hmcgough@eecs.wsu.edu>
2019-11-03 03:41:47 -0800

    Add seven seg pins and test code

--- Test7Seg.py
+++ Test7Seg.py
@@ -48,31 +48,14 @@ def test_all_segments():
 
 def test():
 
-    for i in range(4):
-        test_on(i+1)
-        test_all_segments()
+    while True:
+        for i in range(4):
+            print("Testing panel ", i)
+            test_on(i+1)
+            test_all_segments()
+
     
 
 
 if __name__ == '__main__':
-    s.first_activator.on()
-    s.second_activator.on()
-    s.third_activator.on()
-    s.fourth_activator.off()
-
-    s.top_seg.on()
-    time.sleep(.5)
-    s.top_left.on()
-    time.sleep(.5)
-    s.top_right.on()
-    time.sleep(.5)
-    s.center.on()
-    time.sleep(.5)
-    s.bottom_left.on()
-    time.sleep(.5)
-    s.bottom_right.on()
-    time.sleep(.5)
-    s.bottom.on()
-    time.sleep(.5)
-    s.seperator.on()
-    time.sleep(.5)
+    test()
