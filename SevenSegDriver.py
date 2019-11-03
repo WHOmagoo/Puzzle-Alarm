bd86300c0dfff3bb9c41e8090a0fa8fbca763467
hmcgough <hmcgough@eecs.wsu.edu>
2019-11-03 04:49:02 -0800

    change refresh rate

--- SevenSegDriver.py
+++ SevenSegDriver.py
@@ -94,7 +94,7 @@ class SevenSegDrive:
             self.render_single_number(self.display[i])
             time.sleep(.008)
             self.off()
-            time.sleep(.002)
+            time.sleep(.0008)
 
 
 
