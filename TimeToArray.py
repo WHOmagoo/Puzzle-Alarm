b67d4aa715b52848d78691070b96bdcf96952513
hmcgough <hmcgough@eecs.wsu.edu>
2019-11-03 05:57:45 -0800

    spell second correct

--- TimeToArray.py
+++ TimeToArray.py
@@ -4,7 +4,7 @@ def time_to_array(time):
 
     time_array[0] = (time.minute / 10) % 10
     time_array[1] = time.minute % 10
-    time_array[2] = (time.sescond / 10) % 10
+    time_array[2] = (time.second / 10) % 10
     time_array[3] = time.second % 10
 
     return time_array
